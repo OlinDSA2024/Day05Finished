0ade1911d54fd139a0a78f8c26398d2bfc6c364f
Paul Ruvolo <paullundyruvolo@gmail.com>
2024-01-31 18:21:23 -0500

    first commit

---
+++ src/test/kotlin/MyMutableListTest.kt
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MyMutableListTest {
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun clear() {
+    }
+
+    @Test
+    fun size() {
+    }
+
+    @Test
+    fun get() {
+    }
+
+    @Test
+    fun set() {
+    }
+}
