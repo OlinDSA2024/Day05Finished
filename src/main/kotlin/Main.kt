30423b4b16766d62ad081f7dbb5ad3f044888f36
Paul Ruvolo <paullundyruvolo@gmail.com>
2024-02-01 10:08:16 -0500

    Fixed error in timing code.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,9 @@ import kotlin.time.measureTime
 
 fun main() {
     val arraySizes = listOf(100, 1000, 10000, 100000, 1000000, 10000000, 100000000)
-    val myList = MyMutableIntList()
     println("numberOfElements totalTime timePerElement")
     for (arraySize in arraySizes) {
+        val myList = MyMutableIntList()
         val timeTaken = measureTime {
             for (i in 0..<arraySize) {
                 myList.add(i)
