6069e5701d515df8772fe6bb556de733c6fedc52
Paul Ruvolo <paullundyruvolo@gmail.com>
2024-02-01 09:19:40 -0500

    Changed the list to only work with Ints

--- src/main/kotlin/MyMutableList.kt
+++ src/main/kotlin/MyMutableList.kt
@@ -1,4 +1,62 @@
 package org.example
 
-class MyMutableList {
+import kotlin.reflect.KClass
+
+class MyMutableIntList {
+    private var a = intArrayOf()
+    private var nextSlot = 0
+
+    /**
+     * Grow the array by a multiplicative factor
+     */
+    private fun growArray() {
+        a = if (a.isEmpty()) {
+            IntArray(1)
+        } else {
+            IntArray(a.size*2)
+        }
+    }
+
+    /**
+     * Add [element] to the end of the list
+     */
+    fun add(element: Int) {
+        if (nextSlot >= a.size) {
+            growArray()
+        }
+        a[nextSlot] = element
+        nextSlot++
+    }
+
+    /**
+     * Remove all elements from the list
+     */
+    fun clear() {
+        nextSlot = 0
+    }
+
+    /**
+     * @return the size of the list
+     */
+    fun size(): Int {
+        return nextSlot
+    }
+
+    /**
+     * @parameter index the index to return
+     * @return the element at [index]
+     */
+    operator fun get(index: Int):Int {
+        return a[index]
+    }
+
+    /**
+     * Store [value] at position [index]
+     * @parameter index the index to set
+     * @parameter the value to store at [index]
+     */
+    operator fun set(index: Int, value: Int) {
+        a.set(index, value)
+    }
+
 }
--- src/test/kotlin/MyMutableListTest.kt
+++ src/test/kotlin/MyMutableListTest.kt
@@ -1,3 +1,4 @@
+import org.example.MyMutableIntList
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -6,21 +7,47 @@ class MyMutableListTest {
 
     @Test
     fun add() {
+        val l = MyMutableIntList()
+        l.add(20)
+        for (i in 0..<10) {
+            l.add(i)
+        }
+        assertEquals(l.size(), 11)
+        assertEquals(l[10], 9)
     }
-
     @Test
     fun clear() {
+        val l = MyMutableIntList()
+        l.add(33)
+        for (i in 0..<10) {
+            l.add(i)
+        }
+        l.clear()
+        assertEquals(l.size(), 0)
     }
 
     @Test
     fun size() {
+        val l = MyMutableIntList()
+        l.add(99)
+        for (i in 0..<10) {
+            l.add(i)
+        }
+        assertEquals(l.size(), 11)
     }
 
     @Test
     fun get() {
+        val l = MyMutableIntList()
+        l.add(412)
+        assertEquals(l[0], 412)
     }
 
     @Test
     fun set() {
+        val l = MyMutableIntList()
+        l.add(44)
+        l[0] = -424
+        assertEquals(l[0], -424)
     }
 }
